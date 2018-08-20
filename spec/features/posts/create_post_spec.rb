d9e3ce5ff2b94bc23c0982c44bda03b6ba164bb5
Colyn Prater <colynprater@Colyns-MacBook-Pro.local>
2018-08-20 16:03:26 +0800

    Write test cases with no assertions.

---
+++ spec/features/posts/create_post_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'user creates Post' do
+
+  scenario 'with valid params' do
+    visit new_post_path
+
+    within('form') do
+      fill_in('Title',       with: 'My love for chocolate pudding')
+      fill_in('Description', with: 'Why I love pudding')
+      fill_in('Body',        with: 'It tastes really good and is cheap to buy.')
+
+      click_button('Save')
+    end
+
+    # Add assertions here
+  end
+end
